1db5bc4e60ff0e11a59695be93dff5edbed14c14
Tolga Pirim <tolgapirim25@gmail.com>
2024-08-30 00:33:58 +0300

    add glide for load image and write extension functions

--- app/src/main/java/com/prmto/kekodswitchapp/util/Extensions.kt
+++ app/src/main/java/com/prmto/kekodswitchapp/util/Extensions.kt
@@ -2,13 +2,15 @@ package com.prmto.kekodswitchapp.util
 
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
 import com.google.android.material.switchmaterial.SwitchMaterial
-import com.prmto.kekodswitchapp.switch_main.BottomNavItem
+import com.prmto.kekodswitchapp.ui.switch_main.BottomNavItem
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -16,6 +18,12 @@ import kotlinx.coroutines.launch
 fun SwitchMaterial.setEnableAndUncheck(isEnable: Boolean) {
     isEnabled = isEnable
     isChecked = false
+}
+
+fun ImageView.loadImage(url: String) {
+    Glide.with(this)
+        .load(url)
+        .into(this)
 }
 
 fun Menu.addItem(
