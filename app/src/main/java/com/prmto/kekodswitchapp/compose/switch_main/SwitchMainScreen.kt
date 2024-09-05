88836250fbf9f3d19e87ed6b559799218d346e3d
Tolga Pirim <tolgapirim25@gmail.com>
2024-09-05 23:35:02 +0300

    add lottie anim, gradient to background and new style switch for compose version

--- app/src/main/java/com/prmto/kekodswitchapp/compose/switch_main/SwitchMainScreen.kt
+++ app/src/main/java/com/prmto/kekodswitchapp/compose/switch_main/SwitchMainScreen.kt
@@ -1,14 +1,27 @@
 package com.prmto.kekodswitchapp.compose.switch_main
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.prmto.kekodswitchapp.R
 import com.prmto.kekodswitchapp.SwitchState
 import com.prmto.kekodswitchapp.compose.switch_main.components.SwitchItem
@@ -21,59 +34,89 @@ fun SwitchMainScreen(
     switchStates: List<SwitchState>,
     onCheckedSwitch: (Emotion, Boolean) -> Unit,
 ) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.switch_anim)
+    )
+
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    listOf(
+                        Color(0xFF4CAF50),
+                        Color(0xFF155282)
+                    )
+                )
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        SwitchSection {
-            SwitchItem(
-                switchStates = switchStates,
-                emotion = Emotion.HAPPINESS,
-                switchTitleId = R.string.happinness,
-                onCheckedSwitch = onCheckedSwitch
-            )
 
-            SwitchItem(
-                switchStates = switchStates,
-                emotion = Emotion.OPTIMISTIC,
-                switchTitleId = R.string.optimism,
-                onCheckedSwitch = onCheckedSwitch
-            )
+        LottieAnimation(
+            composition = composition,
+            modifier = Modifier
+                .padding(50.dp)
+                .size(300.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(colorResource(id = R.color.blue)),
+            reverseOnRepeat = true,
+            iterations = LottieConstants.IterateForever
+        )
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SwitchSection {
+                SwitchItem(
+                    switchStates = switchStates,
+                    emotion = Emotion.HAPPINESS,
+                    switchTitleId = R.string.happinness,
+                    onCheckedSwitch = onCheckedSwitch
+                )
 
-
-            SwitchItem(
-                switchStates = switchStates,
-                emotion = Emotion.KINDNESS,
-                switchTitleId = R.string.kidness,
-                onCheckedSwitch = onCheckedSwitch
-            )
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        SwitchSection {
-            SwitchItem(
-                switchStates = switchStates,
-                emotion = Emotion.GIVING,
-                switchTitleId = R.string.giving,
-                onCheckedSwitch = onCheckedSwitch
-            )
-
-            SwitchItem(
-                switchStates = switchStates,
-                emotion = Emotion.RESPECT,
-                switchTitleId = R.string.Respect,
-                onCheckedSwitch = onCheckedSwitch
-            )
+                SwitchItem(
+                    switchStates = switchStates,
+                    emotion = Emotion.OPTIMISTIC,
+                    switchTitleId = R.string.optimism,
+                    onCheckedSwitch = onCheckedSwitch
+                )
 
 
-            SwitchItem(
-                switchStates = switchStates,
-                emotion = Emotion.EGO,
-                switchTitleId = R.string.ego,
-                onCheckedSwitch = onCheckedSwitch
-            )
+                SwitchItem(
+                    switchStates = switchStates,
+                    emotion = Emotion.KINDNESS,
+                    switchTitleId = R.string.kidness,
+                    onCheckedSwitch = onCheckedSwitch
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            SwitchSection {
+                SwitchItem(
+                    switchStates = switchStates,
+                    emotion = Emotion.GIVING,
+                    switchTitleId = R.string.giving,
+                    onCheckedSwitch = onCheckedSwitch
+                )
+
+                SwitchItem(
+                    switchStates = switchStates,
+                    emotion = Emotion.RESPECT,
+                    switchTitleId = R.string.Respect,
+                    onCheckedSwitch = onCheckedSwitch
+                )
+
+
+                SwitchItem(
+                    switchStates = switchStates,
+                    emotion = Emotion.EGO,
+                    switchTitleId = R.string.ego,
+                    onCheckedSwitch = onCheckedSwitch
+                )
+            }
         }
     }
+
 }
--- app/src/main/java/com/prmto/kekodswitchapp/compose/switch_main/components/SwitchItem.kt
+++ app/src/main/java/com/prmto/kekodswitchapp/compose/switch_main/components/SwitchItem.kt
@@ -1,18 +1,25 @@
 package com.prmto.kekodswitchapp.compose.switch_main.components
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prmto.kekodswitchapp.R
 import com.prmto.kekodswitchapp.SwitchState
 import com.prmto.kekodswitchapp.ui.switch_main.Emotion
@@ -26,7 +33,10 @@ fun SwitchItem(
     onCheckedSwitch: (Emotion, Boolean) -> Unit
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = colorResource(id = R.color.gray))
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -43,6 +53,9 @@ fun SwitchItem(
             )
         )
 
-        Text(text = stringResource(id = switchTitleId))
+        Text(
+            text = stringResource(id = switchTitleId),
+            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp)
+        )
     }
 }
